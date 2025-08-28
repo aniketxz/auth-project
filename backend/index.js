861602e81e7d19d9b60b9e5c3ce0f7ced752f7fb
Aniket Kumar Mahato <aniketkumar0x@gmail.com>
2025-08-28 23:32:39 +0530

    backend setup done

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+import express from 'express'
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('API is running...')
+})
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => (
+  console.log(`Server is running on port ${PORT}`)
+))
