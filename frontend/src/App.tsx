a4c838dcc45caab07c94581d2e9e2fd94f283d20
Aniket Kumar Mahato <aniketkumar0x@gmail.com>
2025-08-30 19:12:36 +0530

    (frontend): setup routes, protected homepage route, setup zustand

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
+import NotesPage from './pages/NotesPage'
+import ProtectedRoute from './components/ProtectedRoute'
 import './App.css'
 
 function App() {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path='/signup' element={<SignUp />} />
+				<Route path='/signin' element={<SignIn />} />
 
-  return (
-    <>
-      <h3 className='text-4xl text-neutral-600'>Hello Everynyan</h3>
-    </>
-  )
+				<Route element={<ProtectedRoute />}>
+					<Route path='/' element={<NotesPage />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	)
 }
 
 export default App
